d5213f80066757c0e2af8671f8fd1a4e0e0e6f61
Farid Ariyanto <frdariyanto@gmail.com>
2018-10-24 11:18:27 +0700

    last

---
+++ FARIDARIYANTOSAPUTA_1164036/Soal 10/Soal 10.py
@@ -0,0 +1,16 @@
+import shapefile #mengimpor modul shapefil
+w=shapefile.Writer() #untuk membuat shapefile baru
+w.shapeType #menyeting menggunakan jenis shape apa (point,polygon)
+#membuat dbs dengan 2 field, berupa kolom
+w.field("Nama Bidang","Bidang ke ") 
+w.field("Nama Bidang","Bidang ke") 
+#membuat dbs dengan 2 field, berupa kolom
+w.record("jajargenjang 1","satu") 
+w.record("jajargenjang 2","dua") 
+w.record("jajargenjang 3","tiga") 
+#membuat 3 row karena menggunakan 3
+w.poly(parts=[[[-4,3],[-1,3], [-2,1],[-5,1],[-4,3]]],shapeType=shapefile.POLYGON) #mengisi .shp dengan titik point
+w.poly(parts=[[[2,3],[5,3], [4,1],[1,1], [2,3]]],shapeType=shapefile.POLYGON) #mengisi .shp dengan titik point
+w.poly(parts=[[[-4,-1],[-1,-1], [-2,-3],[-5,-3], [-4,-1]]],shapeType=shapefile.POLYGON) #mengisi .shp dengan titik point
+ 
+w.save("soal10") #melakukan save dengan nama (soal10)
